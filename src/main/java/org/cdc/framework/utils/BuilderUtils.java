29af9cef46289ecb08cb0535f04b7871465fdc5b
cdc12345 <3154934427@qq.com>
2025-05-21 13:03:12 +0800

    fix a bug of comment generator

--- src/main/java/org/cdc/framework/utils/BuilderUtils.java
+++ src/main/java/org/cdc/framework/utils/BuilderUtils.java
@@ -10,68 +10,59 @@ import java.util.stream.Collectors;
 
 public class BuilderUtils {
 
-	public static ProcedureBuilder createOutputProcedure(MCreatorPluginFactory mCreatorPluginFactory, String name,
-			String output) {
-		return mCreatorPluginFactory.createProcedure(name).setOutput(output).setGroup("name").setInputsInline(true);
-	}
+    public static ProcedureBuilder createOutputProcedure(MCreatorPluginFactory mCreatorPluginFactory, String name, String output) {
+        return mCreatorPluginFactory.createProcedure(name).setOutput(output).setGroup("name").setInputsInline(true);
+    }
 
-	/**
-	 * mCreatorPluginFactory.createProcedure(name).setOutput(variableType).setGroup("name").setInputsInline(true)
-	 */
-	public static ProcedureBuilder createOutputProcedure(MCreatorPluginFactory mCreatorPluginFactory, String name,
-			IVariableType variableType) {
-		return mCreatorPluginFactory.createProcedure(name).setOutput(variableType).setGroup("name")
-				.setInputsInline(true);
-	}
+    /**
+     * mCreatorPluginFactory.createProcedure(name).setOutput(variableType).setGroup("name").setInputsInline(true)
+     */
+    public static ProcedureBuilder createOutputProcedure(MCreatorPluginFactory mCreatorPluginFactory, String name, IVariableType variableType) {
+        return mCreatorPluginFactory.createProcedure(name).setOutput(variableType).setGroup("name").setInputsInline(true);
+    }
 
-	/**
-	 * .createProcedure(name).setGroup("name").setPreviousStatement(null).setNextStatement(null).setInputsInline(true)
-	 */
-	public static ProcedureBuilder createCommonProcedure(MCreatorPluginFactory mCreatorPluginFactory, String name) {
-		return mCreatorPluginFactory.createProcedure(name).setGroup("name").setColor(60).setPreviousStatement(null)
-				.setNextStatement(null).setInputsInline(true);
-	}
+    /**
+     * .createProcedure(name).setGroup("name").setPreviousStatement(null).setNextStatement(null).setInputsInline(true)
+     */
+    public static ProcedureBuilder createCommonProcedure(MCreatorPluginFactory mCreatorPluginFactory, String name) {
+        return mCreatorPluginFactory.createProcedure(name).setGroup("name").setColor(60).setPreviousStatement(null).setNextStatement(null).setInputsInline(true);
+    }
 
-	public static ProcedureBuilder createProcedureCategory(MCreatorPluginFactory mCreatorPluginFactory,
-			IProcedureCategory category) {
-		return createProcedureCategory(mCreatorPluginFactory, category.getName());
-	}
+    public static ProcedureBuilder createProcedureCategory(MCreatorPluginFactory mCreatorPluginFactory, IProcedureCategory category) {
+        return createProcedureCategory(mCreatorPluginFactory, category.getName());
+    }
 
-	public static ProcedureBuilder createProcedureCategory(MCreatorPluginFactory mCreatorPluginFactory, String name) {
-		var aitaskscat = mCreatorPluginFactory.createProcedure();
-		if (name != null) {
-			aitaskscat.setName(name);
-		}
-		aitaskscat.markType();
-		return aitaskscat;
-	}
+    public static ProcedureBuilder createProcedureCategory(MCreatorPluginFactory mCreatorPluginFactory, String name) {
+        var aitaskscat = mCreatorPluginFactory.createProcedure();
+        if (name != null) {
+            aitaskscat.setName(name);
+        }
+        aitaskscat.markType();
+        return aitaskscat;
+    }
 
-	public static ProcedureBuilder createAITaskCategory(MCreatorPluginFactory mCreatorPluginFactory, String category) {
-		var pro = mCreatorPluginFactory.createAITask();
-		pro.markType();
-		if (category != null) {
-			pro.setName(category);
-		}
-		return pro;
-	}
+    public static ProcedureBuilder createAITaskCategory(MCreatorPluginFactory mCreatorPluginFactory, String category) {
+        var pro = mCreatorPluginFactory.createAITask();
+        pro.markType();
+        if (category != null) {
+            pro.setName(category);
+        }
+        return pro;
+    }
 
-	public static boolean isSupportProcedure(String generatorName) {
-		return generatorName.startsWith("forge") || generatorName.startsWith("neoforge") || generatorName.startsWith(
-				"fabric") || generatorName.startsWith("spigot");
-	}
+    public static boolean isSupportProcedure(String generatorName) {
+        return generatorName.startsWith("forge") || generatorName.startsWith("neoforge") || generatorName.startsWith("fabric") || generatorName.startsWith("spigot");
+    }
 
-	public static String generateInputsComment(JsonArray inputs) {
-		return inputs.asList().stream().map(a -> "${input$" + a.getAsString() + "}")
-				.collect(Collectors.joining(",", "<#-- ", " -->"));
-	}
+    public static String generateInputsComment(JsonArray inputs) {
+        return inputs.asList().stream().map(a -> "${input$" + a.getAsString() + "}").collect(Collectors.joining(",", "<#- ", " ->"));
+    }
 
-	public static String generateStatementsComment(JsonArray statements) {
-		return statements.asList().stream().map(a -> "${statement$" + a.getAsString() + "}")
-				.collect(Collectors.joining(",", "<#-- ", " -->"));
-	}
+    public static String generateStatementsComment(JsonArray statements) {
+        return statements.asList().stream().map(a -> "${statement$" + a.getAsString() + "}").collect(Collectors.joining(",", "<#- ", " ->"));
+    }
 
-	public static String generateFieldsComment(JsonArray fields) {
-		return fields.asList().stream().map(a -> "${field$" + a.getAsString() + "}")
-				.collect(Collectors.joining(",", "<#-- ", " -->"));
-	}
+    public static String generateFieldsComment(JsonArray fields) {
+        return fields.asList().stream().map(a -> "${field$" + a.getAsString() + "}").collect(Collectors.joining(",", "<#- ", " ->"));
+    }
 }
