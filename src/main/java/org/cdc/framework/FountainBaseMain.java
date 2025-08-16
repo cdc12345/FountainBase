1cd8d81ee126c561fd24fd5233bc0f9eacf0e614
cdc12345 <3154934427@qq.com>
2025-08-16 09:57:51 +0800

    optimise the args using

--- src/main/java/org/cdc/framework/FountainBaseMain.java
+++ src/main/java/org/cdc/framework/FountainBaseMain.java
@@ -4,29 +4,28 @@ import org.cdc.framework.interfaces.IFountainMain;
 import org.cdc.framework.interfaces.annotation.DefaultPluginFolder;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.ServiceLoader;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 public class FountainBaseMain {
 	public static void main(String[] args) {
-		AtomicReference<MCreatorPluginFactory> mCreatorPluginFactoryAtomicReference = new AtomicReference<>(null);
+		ArrayList<MCreatorPluginFactory> mCreatorPluginFactoryArrayList = new ArrayList<>();
 		if (args.length >= 1) {
-			Path pluginDirectory = Paths.get(args[0]);
-			mCreatorPluginFactoryAtomicReference.set(new MCreatorPluginFactory(pluginDirectory.toFile()));
+			mCreatorPluginFactoryArrayList.addAll(Stream.of(args).map(a->new MCreatorPluginFactory(new File(a))).toList());
 		}
 
+		var iterator = mCreatorPluginFactoryArrayList.iterator();
 		ServiceLoader<IFountainMain> serviceLoader = ServiceLoader.load(IFountainMain.class);
 		serviceLoader.stream().forEach(a -> {
 			MCreatorPluginFactory mCreatorPluginFactory1 = null;
-			if (mCreatorPluginFactoryAtomicReference.get() == null) {
+			if (!iterator.hasNext()) {
 				if (a.get().getClass().isAnnotationPresent(DefaultPluginFolder.class)) {
 					mCreatorPluginFactory1 = new MCreatorPluginFactory(new File(a.get().getClass().getAnnotation(
 							DefaultPluginFolder.class).value()));
 				}
 			} else {
-				mCreatorPluginFactory1 = mCreatorPluginFactoryAtomicReference.get();
+				mCreatorPluginFactory1 = iterator.next();
 			}
 			a.get().generatePlugin(mCreatorPluginFactory1);
 		});
