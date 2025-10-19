80a908ae710897a5d5b77744878cf697e0d318e8
cdc12345 <3154934427@qq.com>
2025-10-19 16:11:25 +0800

    update Readme

--- src/test/java/org/cdc/test/ProceduresTest.java
+++ src/test/java/org/cdc/test/ProceduresTest.java
@@ -13,91 +13,99 @@ import java.util.Locale;
 
 public class ProceduresTest {
 
-    private final String pluginPath = "build/plugins";
+	private final String pluginPath = "build/plugins";
 
-    @Test
-    public void typeTest(){
+	@Test public void typeTest() {
 		System.out.println(new File(pluginPath).getAbsolutePath());
-        MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
-        mcr.createProcedureCategory("helloworld").markType().setColor(Color.RED).buildAndOutput();
-        mcr.createProcedure().markType().setName("testtype1").setParentCategory("helloworld").setColor(Color.BLUE).buildAndOutput();
+		MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
+		mcr.createProcedureCategory("helloworld").markType().setColor(Color.RED).buildAndOutput();
+		mcr.createProcedure().markType().setName("testtype1").setParentCategory("helloworld").setColor(Color.BLUE)
+				.buildAndOutput();
 
-        mcr.createAITaskCategory("helloai").setColor(Color.RED).buildAndOutput();
-    }
+		mcr.createAITaskCategory("helloai").setColor(Color.RED).buildAndOutput();
+	}
 
-    @Test
-    public void variableTest(){
-        MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
-        mcr.createVariable().setName("hey").setColor(Color.RED).setNullable(true).setIgnoredByCoverage(true).buildAndOutput();
-        mcr.createVariable().setName("atomicitemstack").setColor(255).setBlocklyVariableType("AtomicItemStack").setNullable(false).setIgnoredByCoverage(true).buildAndOutput();
-    }
+	@Test public void variableTest() {
+		MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
+		mcr.createVariable().setName("hey").setColor(Color.RED).setNullable(true).setIgnoredByCoverage(true)
+				.buildAndOutput();
+		mcr.createVariable().setName("atomicitemstack").setColor(255).setBlocklyVariableType("AtomicItemStack")
+				.setNullable(false).setIgnoredByCoverage(true).buildAndOutput();
+	}
 
-    @Test
-    public void generatorTest() {
-        MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
-        mcr.initGenerator(Generators.FORGE1201);
-        Assertions.assertTrue(Files.exists(Path.of(mcr.rootPath().getPath(), "forge-1.20.1")));
+	@Test public void generatorTest() {
+		MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
+		mcr.initGenerator(Generators.FORGE1201);
+		Assertions.assertTrue(Files.exists(Path.of(mcr.rootPath().getPath(), "forge-1.20.1")));
 
-        mcr.createInfo().setId("test").setAuthor("cdc12345").addSupportedVersion(MCreatorVersions.V_2025_1).setName("test");
-    }
+		mcr.createInfo().setId("test").setAuthor("cdc12345").addSupportedVersion(MCreatorVersions.V_2025_1)
+				.setName("test");
+	}
 
-    @Test
-    public void procedureTest(){
-        MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
-        mcr.createProcedure().setName("hey_set").setColor(Color.RED).setPreviousStatement(null)
-                .setNextStatement(null)
-                .appendArgs0InputValueWithDefaultToolboxInit("hello", BuiltInTypes.Number)
-                .appendArgs0InputValue("placeholder",(String) null).appendRequiredApi("helloworld").initGenerator().buildAndOutput();
-        Assertions.assertThrows(RuntimeException.class, ()-> mcr.createProcedure().buildAndOutput());
+	@Test public void procedureTest() {
+		MCreatorPluginFactory mcr = new MCreatorPluginFactory(new File(pluginPath));
+		mcr.createProcedure().setName("hey_set").setCategory(BuiltInToolBoxId.Procedure.ADVANCED).setColor(Color.RED)
+				.setPreviousStatement(null).setNextStatement(null)
+				.appendArgs0InputValueWithDefaultToolboxInit("hello", BuiltInTypes.Number)
+				.appendArgs0InputValue("placeholder", (String) null).appendArgs0StatementInput("statement")
+				.statementBuilder().appendProvide("test", BuiltInTypes.Number).buildAndReturn()
+				.appendRequiredApi("helloworld").initGenerator().buildAndOutput();
+		Assertions.assertThrows(RuntimeException.class, () -> mcr.createProcedure().buildAndOutput());
 
-        mcr.createProcedure("advancements_clearall").setInputsInline(true).setColor("251").setPreviousStatement(null).setNextStatement(null).setToolBoxId("unsafe").appendDependency("world",BuiltInTypes.World).buildAndOutput();
-        mcr.createProcedure("block_namespace").setInputsInline(true).setColor("%{BKY_TEXTS_HUE}").setOutput(BuiltInTypes.String).setToolBoxId("blockdata").setGroup("name").buildAndOutput();
-        mcr.initGenerator(Generators.FORGE1201);
-    }
+		mcr.createProcedure("advancements_clearall").setInputsInline(true).setColor("251").setPreviousStatement(null)
+				.setNextStatement(null).setToolBoxId("unsafe").appendDependency("world", BuiltInTypes.World)
+				.buildAndOutput();
+		mcr.createProcedure("block_namespace").setInputsInline(true).setColor("%{BKY_TEXTS_HUE}")
+				.setOutput(BuiltInTypes.String).setToolBoxId("blockdata").setGroup("name").buildAndOutput();
+		mcr.initGenerator(Generators.FORGE1201);
+	}
 
-    @Test
-    public void aiTasksTest(){
-        MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
-        mcr.createAITask("hey_am_you").setColor(Color.RED).setPreviousStatement(null).setNextStatement(null).setGroup("name").setInputsInline(true).setToolBoxId(BuiltInToolBoxId.AITasks.COMBAT_TASKS).appendArgs0FieldAIConditionSelector("ai").initGenerator().buildAndOutput();
-        mcr.initGenerator(Generators.FORGE1201);
-    }
+	@Test public void aiTasksTest() {
+		MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
+		mcr.createAITask("hey_am_you").setColor(Color.RED).setPreviousStatement(null).setNextStatement(null)
+				.setGroup("name").setInputsInline(true).setToolBoxId(BuiltInToolBoxId.AITasks.COMBAT_TASKS)
+				.appendArgs0FieldAIConditionSelector("ai").initGenerator().buildAndOutput();
+		mcr.initGenerator(Generators.FORGE1201);
+	}
 
-    @Test
-    public void triggersTest(){
-        MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
-        mcr.createTrigger().setName("hello").appendDependency("name","type").setCancelable(true).setSide(Side.Client).setHasResult(true).initGenerator().buildAndOutput();
-        mcr.createTrigger().setName("effect_applicable").setCancelable(true).setHasResult(true).appendRequiredApi("hey").appendDependency("entity",BuiltInTypes.Entity).appendDependency("world",BuiltInTypes.World).appendDependency("x",BuiltInTypes.Number).appendDependency("y",BuiltInTypes.Number).appendDependency("z",BuiltInTypes.Number).appendDependency("effect","object").buildAndOutput();
-        mcr.initGenerator(Generators.FORGE1201);
-    }
+	@Test public void triggersTest() {
+		MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
+		mcr.createTrigger().setName("hello").appendDependency("name", "type").setCancelable(true).setSide(Side.Client)
+				.setHasResult(true).initGenerator().buildAndOutput();
+		mcr.createTrigger().setName("effect_applicable").setCancelable(true).setHasResult(true).appendRequiredApi("hey")
+				.appendDependency("entity", BuiltInTypes.Entity).appendDependency("world", BuiltInTypes.World)
+				.appendDependency("x", BuiltInTypes.Number).appendDependency("y", BuiltInTypes.Number)
+				.appendDependency("z", BuiltInTypes.Number).appendDependency("effect", "object").buildAndOutput();
+		mcr.initGenerator(Generators.FORGE1201);
+	}
 
-    @Test
-    public void datalistTest(){
-        MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
-        mcr.createDataList().setName("datalist").appendElement("hello").initGenerator().buildAndOutput();
-        mcr.createDataList().setName("types").appendElement("hey","hello").initGenerator().build();
-        mcr.createDataList().setName("blocksitems").appendElement("""
-                external: 
-                   readable_name: "Air"
-                   type: block""", """
-                
-                  - Blocks.AI
-                  - "air"
-                """).initGenerator().buildAndOutput();
-        mcr.initGenerator(Generators.FORGE1201);
-    }
+	@Test public void datalistTest() {
+		MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
+		mcr.createDataList().setName("datalist").appendElement("hello").initGenerator().buildAndOutput();
+		mcr.createDataList().setName("types").appendElement("hey", "hello").initGenerator().build();
+		mcr.createDataList().setName("blocksitems").appendElement("""
+				external: 
+				   readable_name: "Air"
+				   type: block""", """
+				
+				  - Blocks.AI
+				  - "air"
+				""").initGenerator().buildAndOutput();
+		mcr.initGenerator(Generators.FORGE1201);
+	}
 
-    @Test
-    public void langTest(){
-        MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
-        mcr.createDefaultLanguage().appendLocalization("test","test").buildAndOutput();
-        mcr.createLanguage(Locale.getDefault()).appendLocalization("test1","test1").buildAndOutput();
+	@Test public void langTest() {
+		MCreatorPluginFactory mcr = MCreatorPluginFactory.createFactory(pluginPath);
+		mcr.createDefaultLanguage().appendLocalization("test", "test").buildAndOutput();
+		mcr.createLanguage(Locale.getDefault()).appendLocalization("test1", "test1").buildAndOutput();
 
-        Assertions.assertThrows(RuntimeException.class,()-> mcr.createProcedure().setName("test_lang").appendArgs0InputValue("hello","hey").setLanguage(mcr.createDefaultLanguage(),"hey"));
-    }
+		Assertions.assertThrows(RuntimeException.class,
+				() -> mcr.createProcedure().setName("test_lang").appendArgs0InputValue("hello", "hey")
+						.setLanguage(mcr.createDefaultLanguage(), "hey"));
+	}
 
-    @Test
-    public void apisTest(){
-        MCreatorPluginFactory mCreatorPluginFactory = MCreatorPluginFactory.createFactory(pluginPath);
-        mCreatorPluginFactory.createApis("helloworld");
-    }
+	@Test public void apisTest() {
+		MCreatorPluginFactory mCreatorPluginFactory = MCreatorPluginFactory.createFactory(pluginPath);
+		mCreatorPluginFactory.createApis("helloworld");
+	}
 }
